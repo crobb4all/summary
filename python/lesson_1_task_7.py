d64733b8ca62134ee34c2ba83a3b0bcf73cb7e3d
crobb4all <crobb4all>
2024-01-25 19:53:01 +0400

    nice2

---
+++ python/lesson_1_task_7.py
@@ -0,0 +1,38 @@
+def one():
+    print("1") 
+
+def two():
+    print("2")
+
+def three():
+    print("3")
+
+def four():
+    print("4")
+
+def five():
+    print("5")
+
+def six():
+    print("6")
+
+def seven():
+    print("7")
+
+def eight():
+    print("8")
+
+def nine():
+    print("9")
+
+def null():
+    print("0")
+
+
+eight(), eight(), null(), null(), five(), five(), five(), three(), five(), three(), five()
+
+numbers = [eight, eight, null, null, five, five, five, three, five, three, five]
+for line in numbers:
+    line()
+
+# 88005553535
