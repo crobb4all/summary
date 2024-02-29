e768a40fd90d3a96310b9b6cc15cb806494a8dce
crobb4all <crobb4all>
2024-02-29 22:22:57 +0400

    work moment

---
+++ python/Lesson_2/test.py
@@ -0,0 +1,10 @@
+#for x in range(0, 21):
+#   print('x =', x, 'x2 =', x*x)
+
+#user = ['Ivan', 'Semen', 'Otto', 'Lol', 'Arrrar', 'jopa', 'kall']
+
+#l = len(user)
+
+#for i in range(0,2):
+#    print (user[i])
+
---
+++ python/for my project/pet_project_work.py
@@ -0,0 +1,9 @@
+# D=B^2 - 4ac
+
+b = float(input("Input date: "))
+a = float(input("Input date: "))
+c = float(input("Input date: "))
+
+D = (b * b) - (4 * a * c)
+
+print(D)
