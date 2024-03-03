44174d23d01eff7d6bf99ed3d2e1eaba9cb552a5
crobb4all <crobb4all>
2024-03-03 20:33:00 +0400

    task 2 complete

--- python/Lesson_2/lesson_2_task_2.py
+++ python/Lesson_2/lesson_2_task_2.py
@@ -1,5 +1,18 @@
-is_year_leap = int(input("Введите год: "))
+# Version 1
+def is_year_leap():
+    year = int(input("Введите год: "))
+    y = year % 4
+    print('год', year,': ', y == 0)
 
+is_year_leap()
 
+print('end point')
 
-print(is_year_leap)
+# Version 2
+def is_year_leap(t):
+    y = t % 4
+    print('год', t,': ', y == 0)
+
+is_year_leap(2024)
+
+print('end point')
